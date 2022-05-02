5dfc26aa2a5eeb62d001af6aa0dffdf64eac3853
Sebastian Proksch <development@mail.proks.ch>
2022-05-02 04:57:59 +0200

    Initial version of util

---
+++ src/test/java/mylib/RemlaUtilTest.java
@@ -0,0 +1,16 @@
+package mylib;
+
+public class RemlaUtilTest {
+
+//	@Test
+	public void asd() {
+		var v = RemlaUtil.getUtilVersion();
+		System.out.println();
+	}
+
+//	@Test
+	public void hostName() {
+		var h = RemlaUtil.getHostName();
+		System.out.println();
+	}
+}
